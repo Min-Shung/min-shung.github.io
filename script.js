38c49fd3db692f90c8b56e698faa90377610e80c
Min0Shung <rainyseasonmail@gmail.com>
2025-10-19 01:19:07 +0800

    .

--- script.js
+++ script.js
@@ -405,7 +405,7 @@ async function initDemoVideo() {
     if (!loaded) await cacheDemoVideo(demoVideo);
 
     setupPose(demoVideo, demoCanvas, pose2, "pressPath2", "pressTimestamps2", "pressStartTime2");
-    demoVideo.playbackRate = 0.82;
+    demoVideo.playbackRate = 0.86;
     // === 無縫循環 ===
     demoVideo.addEventListener("timeupdate", () => {
     if (demoVideo.duration && demoVideo.currentTime >= demoVideo.duration - 0.08) {
